2258dd3df614f4e5c32538590c8fc5d3a7bd069c
jonas <jonas@mehlhaus.eu>
2013-12-02 00:31:43 +0100

    First idea for profile management

---
+++ entwurf/java/de/croggle/data/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package de.croggle.data;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * 
+ * @opt all
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+	
+		
+		public DatabaseHelper(Context context, String DATABASE_NAME, String DATABASE_VERSION) {
+			
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db, String DATABASE_CREATE) {
+			
+			
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+			
+		}
+	}
+	
+}
