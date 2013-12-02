76a083ea2c650f2c548613068084bebd8e8c71e9
Lukas Boehm <suluke93@gmail.com>
2013-12-02 14:33:43 +0100

    Change edge color and fontsize

--- entwurf/java/de/croggle/UMLOptions.java
+++ entwurf/java/de/croggle/UMLOptions.java
@@ -2,8 +2,17 @@ package de.croggle;
 
 /**
  * @opt all
+ * 
+ * @opt edgecolor #565656
+ * @opt edgefontsize 12
+ * 
  * @opt hide android\.
  * @opt hide SQL
+ * 
+ * I tried the qualified names option, but it generated java.lang.String
+ * opt qualify
+ * opt postfixpackage
+ * 
  * @hidden
  */
 public class UMLOptions {
