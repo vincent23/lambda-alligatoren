68cc700f4c06302f998eed105f09d87675c9fc22
Lukas Boehm <suluke93@gmail.com>
2013-12-03 21:42:17 +0100

    Add PackageUML.java for possibility to better style the graph

--- entwurf/java/UMLOptions.java
+++ entwurf/java/UMLOptions.java
@@ -2,15 +2,14 @@ package de.croggle;
 // package must ALWAYS remain first line for makefile sed to work
 
 /**
+ * @opt views
+ * 
  * @opt all
  * 
  * @opt nodefontname Verdana
  * @opt edgefontname Verdana
  * @opt edgecolor #565656
- * @opt edgefontsize 12
- * 
- * @opt hide android\.
- * @opt hide SQL
+ * @opt edgefontsize 12 
  * 
  * I tried the qualified names option, but it generated java.lang.String
  * opt qualify
---
+++ entwurf/java/de/croggle/PackageUML.java
@@ -0,0 +1,19 @@
+package de.croggle;
+
+/**
+ * @view
+ *
+ * @match class .*
+ * @opt nodefillcolor LightGray
+ *
+ * @match class de.croggle.ui.*
+ * @opt nodefillcolor PaleGreen
+ *
+ * @match class de.croggle.game.*
+ * @opt nodefillcolor LemonChiffon
+ *
+ * @match class java.*|android.*
+ * @opt hide
+ */
+public class PackageUML {
+}
