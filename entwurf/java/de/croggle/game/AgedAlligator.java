5896962953b13b19da1310b89239bdb4e6e85bf0
Lena Winter <lena.k.winter@gmail.com>
2013-12-03 12:50:26 +0100

    Add attributes to AgedAlligator and Alligator.

--- entwurf/java/de/croggle/game/AgedAlligator.java
+++ entwurf/java/de/croggle/game/AgedAlligator.java
@@ -2,7 +2,10 @@ package de.croggle.game;
 
 /**
  * 
+ * @composed 1 - * Family
  */
 public class AgedAlligator implements BoardObject {
+
+	private Family[] families;
 	
 }
--- entwurf/java/de/croggle/game/Alligator.java
+++ entwurf/java/de/croggle/game/Alligator.java
@@ -3,9 +3,10 @@ package de.croggle.game;
 /**
  * @opt all
  * @node class
+ * @composed 1 - * Family
  */
 public class Alligator implements BoardObject {
-    Color color;
-    
+    private Color color;
+    private Family[] families;
 	
-}
+} 
