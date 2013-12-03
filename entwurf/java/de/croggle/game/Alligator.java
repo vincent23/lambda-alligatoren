9bc9d8eea951e09077594bce6b11124f67225b96
Lena Winter <lena.k.winter@gmail.com>
2013-12-03 13:00:12 +0100

    Add compositions.

--- entwurf/java/de/croggle/game/Alligator.java
+++ entwurf/java/de/croggle/game/Alligator.java
@@ -4,6 +4,7 @@ package de.croggle.game;
  * @opt all
  * @node class
  * @composed 1 - * Family
+ * @composed 1 - 1-0 Color
  */
 public class Alligator implements BoardObject {
     private Color color;
--- entwurf/java/de/croggle/game/Egg.java
+++ entwurf/java/de/croggle/game/Egg.java
@@ -3,6 +3,7 @@ package de.croggle.game;
 /**
  * @opt all
  * @node class
+ * @composed 1 - 1-0 Color
  */
 public class Egg implements BoardObject {
 	Color color;
