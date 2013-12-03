178893ccc45ffc954717be7ea5da83379f335231
Lena Winter <lena.k.winter@gmail.com>
2013-12-03 12:52:30 +0100

    Changed Scene into Board.

--- entwurf/java/de/croggle/game/GameController.java
+++ entwurf/java/de/croggle/game/GameController.java
@@ -1,12 +1,12 @@
 package de.croggle.game;
 
 /**
- * @navassoc 1 "" 1 Scene
+ * @navassoc 1 "" 1 Board
  * @navassoc 1 "" 1 Simulator
  * @navassoc 1 "" 1 ColorController
  */
 public class GameController {
-	private Scene scene;
+	private Board board;
 	private Simulator simulator;
 	private ColorController colorController;
     
