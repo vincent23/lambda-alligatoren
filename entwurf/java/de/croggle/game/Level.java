99f0d49805924d805a8ce676a6473dcc16cffb21
Lena Winter <lena.k.winter@gmail.com>
2013-12-03 12:40:47 +0100

    Add muliplicities.

--- entwurf/java/de/croggle/game/Level.java
+++ entwurf/java/de/croggle/game/Level.java
@@ -4,21 +4,22 @@ import de.croggle.data.Animation;
 /**
  * @opt all
  * @node class
+ * @composed 1 - 3 Board
  */
 public abstract class Level {
     private int levelIndex;
     private int packageIndex;
     
-    private LambdaConstellation inputConstellation;
-    private LambdaConstellation goalConstellation;
-    private LambdaConstellation currentConstellation;
+    private Board inputConstellation;
+    private Board goalConstellation;
+    private Board currentConstellation;
     private boolean animation; //TODO: ein boolean ob da eine Animation ist, und wenn ja, dann das handeln OR einfach eine Animation und wenn keine da ist NULL oder so
     
 	public int getPackageIndex();
 	public int getLevelIndex();
 	
 	//LevelState state; //?
-	public Scene getInputConstellation();
-	public Scene getGoalConstellation();
+	public Board getInputConstellation();
+	public Board getGoalConstellation();
 	public Animation getAnimation();
 }
