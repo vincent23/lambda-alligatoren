c4d0618f07547efa1cc907f45ee2cb6a2d198052
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-07 11:44:17 +0100

    Merge data.model.Settings into data.model.Profile

--- entwurf/java/de/croggle/data/model/Profile.java
+++ entwurf/java/de/croggle/data/model/Profile.java
@@ -12,8 +12,12 @@ public class Profile {
 	private long id;
 	private String name;
 	private String picturePath;
+	private float volumeMusic;
+	private float volumeEffects;
+	private boolean zoomEnabled;
+	private boolean colorblindEnabled;
 
-	public Profile(String name, String picturePath) {
+	public Profile(String name, String picturePath, float volumeMusic, float volumeEffects, boolean zoomEnabled, boolean colorblindEnabled) {
 	}
 
 	protected Profile(Cursor cursor) {
@@ -37,5 +41,37 @@ public class Profile {
 
 	public void setPicturePath(String picturePath) {
 		this.picturePath = picturePath;
+	}
+
+	public float getVolumeMusic() {
+		return volumeMusic;
+	}
+
+	public void setVolumeMusic(float volumeMusic) {
+		this.volumeMusic = volumeMusic;
+	}
+
+	public float getVolumeEffects() {
+		return volumeEffects;
+	}
+
+	public void setVolumeEffects(float volumeEffects) {
+		this.volumeEffects = volumeEffects;
+	}
+
+	public boolean isZoomEnabled() {
+		return zoomEnabled;
+	}
+
+	public void setZoomEnabled(boolean zoomEnabled) {
+		this.zoomEnabled = zoomEnabled;
+	}
+
+	public boolean isColorblindEnabled() {
+		return colorblindEnabled;
+	}
+
+	public void setColorblindEnabled(boolean colorblindEnabled) {
+		this.colorblindEnabled = colorblindEnabled;
 	}
 }
