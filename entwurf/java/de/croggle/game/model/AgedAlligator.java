3a84560e5b0e77ff9d1d8ac1e56a1fcd7abe2cb4
Lena Winter <lena.k.winter@gmail.com>
2013-12-09 16:33:14 +0100

    Added assosiation to ColoredAlligator and AgedAlligator

--- entwurf/java/de/croggle/game/model/AgedAlligator.java
+++ entwurf/java/de/croggle/game/model/AgedAlligator.java
@@ -1,9 +1,12 @@
 package de.croggle.game.model;
 
 /**
- * 
+ *
+ * @has 1 - * BoardObject
  */
 public class AgedAlligator extends Family implements InternalBoardObject {
+
+	private List<BoardObject> children;
 
 	
 }
--- entwurf/java/de/croggle/game/model/ColoredAlligator.java
+++ entwurf/java/de/croggle/game/model/ColoredAlligator.java
@@ -1,7 +1,9 @@
 package de.croggle.game.model;
 /**
  * @composed 1 - 1-0 Color
+ * @has 1 - * BoardObject
  **/
 public class ColoredAlligator extends Family implements InternalBoardObject {
 	private Color color;
+	private List<BoardObject> children;
 }
