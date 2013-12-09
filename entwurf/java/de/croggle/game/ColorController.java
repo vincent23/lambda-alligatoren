195e8989b85e1a6b5397c3378a52be5c3b0339de
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-09 10:40:45 +0100

    delete things previously just commented out

--- entwurf/java/de/croggle/game/ColorController.java
+++ entwurf/java/de/croggle/game/ColorController.java
@@ -10,39 +10,5 @@ import de.croggle.game.model.Color;
 public class ColorController {
 	private ArrayList<Color> usableColors = new ArrayList<Color>();
     private ArrayList<Color> bannedColors = new ArrayList<Color>();
-
-	
-    /**
-	* Add color to usableColors[].
-	*
-	**/
-//    public void addUsableColor(Color color) {
-//        //Check first if color is already in usableColors[].
-//    }
-
-    /**
-	* Add color to bannedColors[].
-	*
-	**/
-//    public void addBannedColor(Color color) {
-//        //Check first if color is already in bannedColors[].
-//    }
-
-	/**
-	* Remove color from usableColors[].
-	*
-	**/
-//    public void removeUsableColor(Color color) {
-//        //Check first if color is not in UsableColors[].
-//    }
-
-	/**
-	* Remove color from bannedColors[].
-	*
-	**/
-//    public void removeBannedColor(Color color) {
-//         //Check first if color is not in BannedColors[].
-//    } */
-    
     
 }
