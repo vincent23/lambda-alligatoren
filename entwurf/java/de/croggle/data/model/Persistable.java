886db607cb2d2dcfd21dedec7b5ef2ac8f4512e1
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-10 10:35:24 +0100

    Add javadoc to data.model.Persistable

--- entwurf/java/de/croggle/data/model/Persistable.java
+++ entwurf/java/de/croggle/data/model/Persistable.java
@@ -2,6 +2,15 @@ package de.croggle.data.model;
 
 import android.database.sqlite.SQLiteDatabase;
 
+/**
+ * Interface for objects which can be saved in the database.
+ */
 public interface Persistable {
+	/**
+	 * Saves the objects state in the database.
+	 *
+	 * @param database A writeable database object.
+	 * @return True if the object was persisted, false otherwise.
+	 */
 	public boolean persist(android.database.sqlite.SQLiteDatabase database);
 }
