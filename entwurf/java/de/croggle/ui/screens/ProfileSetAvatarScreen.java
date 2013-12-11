a04680fffeeb83c996f50f12703cca67a776c80f
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-11 19:16:54 +0100

    fix stupid mistake on my part

--- entwurf/java/de/croggle/ui/screens/ProfileSetAvatarScreen.java
+++ entwurf/java/de/croggle/ui/screens/ProfileSetAvatarScreen.java
@@ -1,7 +1,7 @@
 package de.croggle.ui.screens;
 
 /**
- * Screen, which is used for both creating a new account with a given name as well as changing the name of an existing account.
+ * Screen, which is used for both creating a new account with a given avatar as well as changing the avatar of an existing account.
  * For reference see "Pflichtenheft 10.5.14 / Abbildung 23"
  */
 public class ProfileSetAvatarScreen extends AbstractScreen{
