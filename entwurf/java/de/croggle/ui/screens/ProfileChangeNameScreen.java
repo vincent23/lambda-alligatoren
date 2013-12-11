5562f8865693159b21365bbfa7afeabcba9d4ad2
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-11 19:13:01 +0100

    Ensure consistency between Entwurf and Pflichtenheft

--- entwurf/java/de/croggle/ui/screens/ProfileChangeNameScreen.java
+++
@@ -1,9 +0,0 @@
-package de.croggle.ui.screens;
-
-/**
- * 
- * For reference see "Pflichtenheft 10.5.13 / Abbildung 22"
- */
-public class ProfileChangeNameScreen extends AbstractScreen{
-	
-}
---
+++ entwurf/java/de/croggle/ui/screens/ProfileSetNameScreen.java
@@ -0,0 +1,9 @@
+package de.croggle.ui.screens;
+
+/**
+ * Screen, which is used for both creating a new account as well as changing the name of an existing account.
+ * For reference see "Pflichtenheft 10.5.13 / Abbildung 22"
+ */
+public class ProfileSetNameScreen extends AbstractScreen{
+	
+}
