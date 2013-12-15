961b83df0d49bbf3004c019c7e09f2d46363c8c2
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-15 15:53:36 +0100

    Add references to listeners in the model

--- entwurf/java/de/croggle/model/Egg.java
+++ entwurf/java/de/croggle/model/Egg.java
@@ -1,9 +1,13 @@
 package de.croggle.model;
 
+import de.croggle.renderer.BoardObjectChangedListener;
+
 /**
  * An egg represents a variable within the Lambda Calculus. If the guarding alligator eats something, the eaten thing will hatch from the egg.
  * @composed 1 - 1-0 Color
+ * @has 1 - 1 BoardObjectChangedListener
  */
 public class Egg implements InternalBoardObject {
-	Color color;
+	private Color color;
+	private BoardObjectChangedListener actor;
 }
--- entwurf/java/de/croggle/model/Parent.java
+++ entwurf/java/de/croggle/model/Parent.java
@@ -3,9 +3,12 @@ package de.croggle.model;
 import java.util.List;
 import java.util.Iterator;
 
+import de.croggle.renderer.ChildChangedListener;
+
 /**
  *
  * @has 1 - * InternalBoardObject
+ * @has 1 - 1 ChildChangedListener
 **/
 
 public abstract class Parent {
@@ -13,21 +16,21 @@ public abstract class Parent {
 	 * 
 	 */
 	private List<InternalBoardObject> children;
-	
+	private ChildChangedListener actor;
+
 	/**
 	 * 
 	 */
 	public void addChild(InternalBoardObject child) {
-		
 	}
-	
+
 	/**
 	 * 
 	 */
 	public boolean removeChild(InternalBoardObject child) {
 		return false;
 	}
-	
+
 	/**
 	 * 
 	 */
