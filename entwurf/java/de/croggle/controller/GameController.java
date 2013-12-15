2d91f83b0daa2bdfffc0578599e7111e34246dab
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-15 17:28:09 +0100

    Repositioning methods

--- entwurf/java/de/croggle/controller/GameController.java
+++ entwurf/java/de/croggle/controller/GameController.java
@@ -7,5 +7,19 @@ package de.croggle.controller;
 public class GameController {
 	private Simulator simulator;
 	private ColorController colorController;
+
+    /**
+     * Called when the level is completed. Writes the important results into the database and eventually tells the achievement controller which achievements were achieved.
+     *
+     */
+    public void onCompletedLevel() {
+    }
+    /**
+     * Called to load a new level. With both the packageIndex and the levelIndex one can distinctively indentify the class.
+     * @param packageIndex specifies the level package from which the level is supposed to be loaded
+     * @param levelIndex is the id of the level within the package.
+     */
+    public void loadLevel( int packageIndex, int levelIndex) {
+    }
     
 }
--- entwurf/java/de/croggle/controller/LevelController.java
+++ entwurf/java/de/croggle/controller/LevelController.java
@@ -4,17 +4,4 @@ package de.croggle.controller;
  * @navassoc 1 - * de.croggle.model.Level
  */
 public class LevelController {
-	/**
-     * Called when the level is completed. Writes the important results into the database and eventually tells the achievement controller which achievements were achieved.
-     *
-     */
-    public void onCompletedLevel() {
-    }
-    /**
-     * Called to load a new level. With both the packageIndex and the levelIndex one can distinctively indentify the class.
-     * @param packageIndex specifies the level package from which the level is supposed to be loaded
-     * @param levelIndex is the id of the level within the package.
-     */
-    public void loadLevel( int packageIndex, int levelIndex) {
-    }
 }
