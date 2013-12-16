c21741c797cbaba38c72b0ff5a215c0e1cd0e4c6
Lukas Boehm <suluke93@gmail.com>
2013-12-16 18:39:29 +0100

    Change colors. Especially emphasize exceptions

--- entwurf/java/uml.java
+++ entwurf/java/uml.java
@@ -27,21 +27,34 @@ class UMLOptions {
  * @match class de.croggle.*
  * @opt all
  * @opt nodefillcolor 2
- * 
- * @match class de.croggle.ui.*
- * @opt nodefillcolor 3
  *
  * @match class de.croggle.controller.*
+ * @opt nodefillcolor 3
+ * 
+ * @match class de.croggle.data.*
  * @opt nodefillcolor 4
  * 
- * @match class de.croggle.data.manager.*
+ * @match class de.croggle.model.*
  * @opt nodefillcolor 5
  * 
  * @match class de.croggle.renderer.*
  * @opt nodefillcolor 6
  * 
- * @match class de.croggle.util.*
+ * @match class de.croggle.ui.*
  * @opt nodefillcolor 7
+ * 
+ * @match class de.croggle.util.*
+ * @opt nodefillcolor 8
+ * 
+ * @match class de.croggle.controller.event.*
+ * @opt nodefillcolor 9
+ * 
+ * @match class de.croggle.data.model.*
+ * @opt nodefillcolor 10
+ * 
+ * @match class de.croggle\..*Exception
+ * @opt nodefillcolor firebrick
+ * @opt nodefontcolor white
  * 
  * @match class java.*
  * @opt hide
