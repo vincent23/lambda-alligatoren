aab2bc2d0aa9d664be69b3de00fa271524eca8dc
suluke <suluke93@gmail.com>
2013-12-18 14:56:35 +0100

    Add additional constructors to Exception

--- entwurf/java/de/croggle/controller/IllegalBoardException.java
+++ entwurf/java/de/croggle/controller/IllegalBoardException.java
@@ -6,5 +6,18 @@ import java.lang.Exception;
 * The exception is thrown, when the alligator constellation does not represent a correct lambda term after the simulation has been started.
 */
 public class IllegalBoardException extends Exception{
-
+	
+	/**
+	 * @inheritDoc
+	 */
+	public IllegalBoardException() {
+		super();
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public IllegalBoardException(String message) {
+		super(message);
+	}
 }
--- entwurf/java/de/croggle/controller/ProfileOverflowException.java
+++ entwurf/java/de/croggle/controller/ProfileOverflowException.java
@@ -6,5 +6,18 @@ import java.lang.Exception;
 * The exception is thrown, when there are more than the 6 possible profiles.
 */
 public class ProfileOverflowException extends Exception{
-
+	
+	/**
+	 * @inheritDoc
+	 */
+	public ProfileOverflowException() {
+		super();
+	}
+	
+	/**
+	 * @inheritDoc
+	 */
+	public ProfileOverflowException(String message) {
+		super(message);
+	}
 }
