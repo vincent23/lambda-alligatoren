ea05205066ce05049a98dff779d003823149001d
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-19 12:37:08 +0100

    Add missing import to BoardObjectVisitor

--- entwurf/java/de/croggle/game/visitor/BoardObjectVisitor.java
+++ entwurf/java/de/croggle/game/visitor/BoardObjectVisitor.java
@@ -3,6 +3,7 @@ package de.croggle.game.visitor;
 import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
+import de.croggle.game.board.Board;
 
 /**
  * 
