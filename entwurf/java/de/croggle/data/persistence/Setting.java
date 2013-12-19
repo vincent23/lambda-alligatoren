c2e87e1c1a6916a0793767d57acccfb264f55c6e
jonas <jonas@mehlhaus.eu>
2013-12-20 00:09:01 +0100

    Add javadoc.

--- entwurf/java/de/croggle/data/persistence/Setting.java
+++ entwurf/java/de/croggle/data/persistence/Setting.java
@@ -6,39 +6,106 @@ package de.croggle.data.persistence;
  */
 public class Setting {
 	
+	/**
+	 * The volume of the music played in the game.
+	 */
 	private float volumeMusic;
+	
+	/**
+	 * The volume of the effects used in the game.
+	 */
 	private float volumeEffects;
+	
+	/**
+	 * Determines whether the zoom button is enabled or not.
+	 */
 	private boolean zoomEnabled;
+	
+	/**
+	 * Determines whether colorblind mode is enabled or not.
+	 */	
 	private boolean colorblindEnabled;
 	
+	/**
+	 * 
+	 * Construct a new LevelProgress based on it's properties.
+	 *
+	 * @param volumeMusic The volume of the music.
+	 * @param volumeEffect The volume of the effects.
+	 * @param zoomEnabled Determines whether the zoom button is enabled or not.
+	 * @param colorblindEnabled Determines whether colorblind mode is enabled or not.
+	 */
+	public Setting(float volumeMusic, float volumeEffect, boolean zoomEnabled, boolean colorblindEnabled) {
+		
+	}
+	
+	/**
+	 * Creates a new default setting.
+	 */
+	public Setting() {
+		
+	}
+	
+	/**
+	 * Get the music volume.
+	 * @return The music volume.
+	 */
 	public float getVolumeMusic() {
 		return volumeMusic;
 	}
-
+	
+	/**
+	 * Set the music volume.
+	 * @param volumeMusic The music volume.
+	 */
 	public void setVolumeMusic(float volumeMusic) {
 		this.volumeMusic = volumeMusic;
 	}
 
+	/**
+	 * Get the effect volume.
+	 * @return The volume of the effects.
+	 */
 	public float getVolumeEffects() {
 		return volumeEffects;
 	}
 
+	/**
+	 * Set the effect volume.
+	 * @return The volume of the effects.
+	 */
 	public void setVolumeEffects(float volumeEffects) {
 		this.volumeEffects = volumeEffects;
 	}
 
+	/**
+	 * Get whether zoom button is enabled or not.
+	 * @return If the oom button is enabled true is returned, else false is returned.
+	 */
 	public boolean isZoomEnabled() {
 		return zoomEnabled;
 	}
 
+	/**
+	 * Set the whether zoom button is enabled or not.
+	 * @return True if zoom button is enabled, false otherwise.
+	 */
 	public void setZoomEnabled(boolean zoomEnabled) {
 		this.zoomEnabled = zoomEnabled;
 	}
 
+	/**
+	 * Get whether the colorblind mode is enabled or not.
+	 * @return If the colorblind mode is enabled true is returned, else false is returned.
+	 */
 	public boolean isColorblindEnabled() {
 		return colorblindEnabled;
 	}
 
+	/**
+	 * Set the whether the colorblind mode is enabled or not.
+	 * @return True if the colorblind mode is enabled, false otherwise.
+	 */
 	public void setColorblindEnabled(boolean colorblindEnabled) {
 		this.colorblindEnabled = colorblindEnabled;
 	}
--- entwurf/java/de/croggle/data/persistence/Statistic.java
+++ entwurf/java/de/croggle/data/persistence/Statistic.java
@@ -6,26 +6,67 @@ package de.croggle.data.persistence;
  */
 public class Statistic {
 	
+	/**
+	 * The amount of time a user has played the game.
+	 */
 	private int playtime;
+	
+	/**
+	 * The number of hints used by the user.
+	 */
 	private int usedHints;
+	
+	/**
+	 * The number of levels the user has completed.
+	 */
 	private int levelsComplete;
+	
+	/**
+	 * The number of packages the user has completed.
+	 */
 	private int packagesComplete;
 	
-	private int 	resetsUsed;
-    private int 	recolorings;
+	/**
+	 * The number of resets done by the user.
+	 */
+	private int resetsUsed;
+	
+	/**
+	 * The number of recoloring actions performed by the user.
+	 */
+    private int recolorings;
 
-	
+    /**
+	 * The number of alligators eaten during beta reductions.
+	 */
 	private int alligatorsEaten;
+	
+	/**
+	 * The number of alligators placed in the placement mode.
+	 */
 	private int alligatorsPlaced;
+	
+	/**
+	 * The number of eggs hatched during beta reductions.
+	 */
 	private int eggsHatched;
+	
+	/**
+	 * The number of eggs placed in the placement mode.
+	 */
 	private int eggsPlaced;
-	
-	// ...
-	
+
+	/**
+	 * Creates a new default statistic.
+	 */
 	public Statistic() {
 		
 	}
 
+	/**
+	 * Get the playtime.
+	 * @return The playtime is returned.
+	 */
 	public int getPlaytime() {
 		return playtime;
 	}
@@ -34,37 +75,151 @@ public class Statistic {
 		return usedHints;
 	}
 
+	/**
+	 * Get the number of completed levels.
+	 * @return The number of completed levels.
+	 */
 	public int getLevelsComplete() {
 		return levelsComplete;
 	}
 
+	/**
+	 * Get the number of completed packages.
+	 * @return The number of completed packages.
+	 */
 	public int getPackagesComplete() {
 		return packagesComplete;
 	}
 
+	/**
+	 * Get the number of resets used.
+	 * @return The number of used resets.
+	 */
 	public int getResetsUsed() {
 		return resetsUsed;
 	}
 
+	/**
+	 * Get the number of recoloring actions.
+	 * @return The number of recoloring actions.
+	 */
 	public int getRecolorings() {
 		return recolorings;
 	}
 
+	/**
+	 * Get the total number of alligators eaten during beta reductions.
+	 * @return The number of eaten alligators.
+	 */
 	public int getAlligatorsEaten() {
 		return alligatorsEaten;
 	}
 
+	/**
+	 * Get the total number of alligators placed in the placement mode.
+	 * @return The number of eaten alligators.
+	 */
 	public int getAlligatorsPlaced() {
 		return alligatorsPlaced;
 	}
 
+	/**
+	 * Get the total number of eggs hatched during beta reductions.
+	 * @return The number of eaten alligators.
+	 */
 	public int getEggsHatched() {
 		return eggsHatched;
 	}
-
+	
+	/**
+	 * Get the total number of eggs placed in the placement mode.
+	 * @return The number of eaten alligators.
+	 */
 	public int getEggsPlaced() {
 		return eggsPlaced;
 	}
+
+	/**
+	 * Set the playtime.
+	 * @param playtime The new playtime.
+	 */
+	public void setPlaytime(int playtime) {
+		this.playtime = playtime;
+	}
+
+	/**
+	 * Set the number of hints used.
+	 * @param playtime The new number of hints used.
+	 */
+	public void setUsedHints(int usedHints) {
+		this.usedHints = usedHints;
+	}
+
+	/**
+	 * Set the number of completed levels.
+	 * @param playtime The new number of completed levels.
+	 */
+	public void setLevelsComplete(int levelsComplete) {
+		this.levelsComplete = levelsComplete;
+	}
+
+	/**
+	 * Set the number of completed packages.
+	 * @param playtime The new number of completed packages.
+	 */
+	public void setPackagesComplete(int packagesComplete) {
+		this.packagesComplete = packagesComplete;
+	}
+
+	/**
+	 * Set the number of resets used.
+	 * @param playtime The new amount of resets used.
+	 */
+	public void setResetsUsed(int resetsUsed) {
+		this.resetsUsed = resetsUsed;
+	}
+
+	/**
+	 * Set the number of recoloring actions.
+	 * @param playtime The new amount of recoloring actions.
+	 */
+	public void setRecolorings(int recolorings) {
+		this.recolorings = recolorings;
+	}
+
+	/**
+	 * Set the number of eaten alligators.
+	 * @param playtime The new number of eaten alligators.
+	 */
+	public void setAlligatorsEaten(int alligatorsEaten) {
+		this.alligatorsEaten = alligatorsEaten;
+	}
+
+	/**
+	 * Set the number of placed alligators.
+	 * @param playtime The new number of placed alligators.
+	 */
+	public void setAlligatorsPlaced(int alligatorsPlaced) {
+		this.alligatorsPlaced = alligatorsPlaced;
+	}
+
+	/**
+	 * Set the number of hatched eggs.
+	 * @param playtime The new number of hatched eggs.
+	 */
+	public void setEggsHatched(int eggsHatched) {
+		this.eggsHatched = eggsHatched;
+	}
+
+	/**
+	 * Set the number of hatched placed.
+	 * @param playtime The new number of hatched placed.
+	 */
+	public void setEggsPlaced(int eggsPlaced) {
+		this.eggsPlaced = eggsPlaced;
+	}
+
+
 
 	
 
