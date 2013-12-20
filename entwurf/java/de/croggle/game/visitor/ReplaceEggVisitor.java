eb74c354efe1ecaadabfadf86d76cee972180e3e
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-20 17:50:49 +0100

    Add paramaters to constructor of ReplaceEggVisitor

--- entwurf/java/de/croggle/game/visitor/ReplaceEggVisitor.java
+++ entwurf/java/de/croggle/game/visitor/ReplaceEggVisitor.java
@@ -1,6 +1,8 @@
 package de.croggle.game.visitor;
 
 import de.croggle.game.event.BoardEventMessenger;
+import de.croggle.game.Color;
+import de.croggle.game.board.InternalBoardObject;
 
 /**
  * A visitor replacing eggs of a certain color with copies of a given 
@@ -9,9 +11,9 @@ import de.croggle.game.event.BoardEventMessenger;
  *
  */
 public class ReplaceEggVisitor implements BoardObjectVisitor {
-private BoardEventMessenger boardMessenger;
+	private BoardEventMessenger boardMessenger;
 
-public ReplaceEggVisitor(BoardEventMessenger boardMessenger){
-}
-	
+	public ReplaceEggVisitor(Color eggColor, InternalBoardObject bornFamily, BoardEventMessenger boardMessenger){
+	}
+
 }
