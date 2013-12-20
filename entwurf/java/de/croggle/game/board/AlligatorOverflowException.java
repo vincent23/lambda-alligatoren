ef013bd3a1ca366e80d68d4f07180ca834b41897
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-20 17:41:29 +0100

    Fix conditional in throw

--- entwurf/java/de/croggle/game/board/AlligatorOverflowException.java
+++ entwurf/java/de/croggle/game/board/AlligatorOverflowException.java
@@ -3,7 +3,7 @@ package de.croggle.game.board;
 import java.lang.Exception;
 
 /**
-* The exception is thrown, when a lambda term contains more than 300 InternalBoardObjects. This happens while adding alligators or eggs to the working set, e.g. in the sandbox mode.
+* The exception is thrown, when ever a lambda term contains more than 300 InternalBoardObjects. This happens while adding alligators or eggs to the working set, e.g. in the sandbox mode.
 */
 public class AlligatorOverflowException extends Exception{
 	
