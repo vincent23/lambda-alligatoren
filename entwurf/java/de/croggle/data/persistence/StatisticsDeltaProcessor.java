c2ad3980cd8b2c9b8eff39c7c32defe07c385012
Iris M <iris.mehrbrodt@gmx.de>
2013-12-20 16:32:29 +0100

    Comment StatisticsDeltaProcessor

--- entwurf/java/de/croggle/data/persistence/StatisticsDeltaProcessor.java
+++ entwurf/java/de/croggle/data/persistence/StatisticsDeltaProcessor.java
@@ -1,12 +1,18 @@
 package de.croggle.data.persistence;
 
-
 /**
- * 
+ * A listener that is to be implemented by all classes that need to calculate
+ * stuff from the statistic values that occur during a level.
  */
 public interface StatisticsDeltaProcessor {
-    /**
-     * enables different classes to see what changed during one specific level and evaluate the given changes. e.g. Update database of statistics.
-     */
-    public void processDelta(Statistic statisticsDelta);
+	/**
+	 * Evaluates and processes the statistic changes that occurred during a
+	 * level, e.g. updates the database. The changes are packed as a Statistic
+	 * object. The statisticsDelta is changed afterwards, so no references to it
+	 * should be hold.
+	 * 
+	 * @param statisticsDelta
+	 *            the packed statistic changes
+	 */
+	public void processDelta(Statistic statisticsDelta);
 }
