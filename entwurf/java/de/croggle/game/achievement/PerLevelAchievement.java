efb537468c455318dabfb926811279ab766e9b84
Iris M <iris.mehrbrodt@gmx.de>
2013-12-20 12:42:47 +0100

    Rename PerLevelAchievement

--- entwurf/java/de/croggle/game/achievement/PerLevelAchievement.java
+++ entwurf/java/de/croggle/game/achievement/PerLevelAchievement.java
@@ -4,7 +4,7 @@ package de.croggle.game.achievement;
 /**
  * Achievement for passing certain, specified goals within a level, e.g. placing more than 10 Alligators within one level or 5 eggs hatched within one level.
  */
-public class ProLevelAchievement extends Achievement{
+public class PerLevelAchievement extends Achievement{
 
     /**
      * Returns true if the achievement with the given index has been achieved, otherwise false.
