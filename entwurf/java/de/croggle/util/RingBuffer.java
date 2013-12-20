d4cfd88307a912e9912891fa96eeb96f3beab562
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-20 15:31:41 +0100

    Add JavaDoc to Ringbuffer.java

--- entwurf/java/de/croggle/util/RingBuffer.java
+++ entwurf/java/de/croggle/util/RingBuffer.java
@@ -1,19 +1,27 @@
 package de.croggle.util;
 
 /**
- * 
+ * utility construct, which simplifies saving the last 30 steps immensely.
  */
 public class RingBuffer<T> {
 	
 	private T[] buffer;
 	private int head = 0;
 	private int items = 0;
-	
+
+    /**
+     * Creates a ringbuffer with a given size.
+     * @param size the size with which the ringbuffer will be created. 30 in our case.
+     */
 	public RingBuffer (int size) {
 		assert size > 0 : "Cannot initialize RingBuffer with size smaller 0.";
 		this.buffer = new T[size];
 	}
-	
+
+    /**
+     * places given object on top of the ringbuffer.
+     * @param obj the object, which will be placed upon the ringbuffer.
+     */
 	public void push (T obj) {
 		if (items < buffer.length) {
 			items++;
@@ -21,7 +29,10 @@ public class RingBuffer<T> {
 		buffer [head] = obj;
 		head = (head + 1) % buffer.length;
 	}
-	
+	/**
+     * removes the uppermost object of the ringbuffer and returns it.
+     * @return the object, which used to be on top of the ringbuffer.
+     */
 	public T pop () {
 		if (items == 0) {
 			throw new Exception("");
