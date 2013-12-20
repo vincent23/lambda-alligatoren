afe205a4890cc6fd9c01422898afc664076bd91a
Lena Winter <lena.k.winter@gmail.com>
2013-12-20 13:07:41 +0100

    Add javadocs to parent.

--- entwurf/java/de/croggle/game/board/Parent.java
+++ entwurf/java/de/croggle/game/board/Parent.java
@@ -5,24 +5,25 @@ import java.util.Iterator;
 
 
 /**
- *
+ * Parent is an abstract class to model the functions board objects, which can be parents of families, must have.
  * @has 1 - * InternalBoardObject
 **/
 
 public abstract class Parent {
-	/**
-	 * 
-	 */
+
 	private List<InternalBoardObject> children;
 
 	/**
-	 * 
+	 * Adds a child to the familiy of the parent.
+	 * @param child is the InternalBoardObject, which is added to the family of the parent.
 	 */
 	public void addChild(InternalBoardObject child) {
 	}
 
 	/**
-	 * 
+	 * Removes a child from the family of the parent.
+	 * @param child is the InternalBoardObject, which is removed from the family of the parent.
+	 * @return whether the removement was successfull.
 	 */
 	public boolean removeChild(InternalBoardObject child) {
 		return false;
