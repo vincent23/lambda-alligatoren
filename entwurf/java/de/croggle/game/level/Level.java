17342e59379cc42bb4e2974d2bfb3d37182cd586
Lena Winter <lena.k.winter@gmail.com>
2013-12-20 12:19:12 +0100

    Add missing methods and attributs to level.

--- entwurf/java/de/croggle/game/level/Level.java
+++ entwurf/java/de/croggle/game/level/Level.java
@@ -6,38 +6,98 @@ import de.croggle.game.Color;
 import de.croggle.game.board.Board;
 
 /**
+ * This class represents the concept of a level within the game. 
  * @composed 1 - 3 de.croggle.game.board.Board
  */
 public abstract class Level {
 	private int levelIndex;
 	private int packageIndex;
 
-	private Board inputBoard;
+	private Board initialBoard;
 	private Board goalBoard;
 	private Animation animation;
 
-	private Color userColors[];
+	private Color[] userColors;
+	private String hint;
+	private String description;
+	private int abortSimulationAfter;
+	
+	/**
+	 * Gets the index of the levelpackage this level belongs to. 
+	 * @return the index of the level package.
+	 */
 
 	public int getPackageIndex() {
 		return 0;
 	}
 
+	/**
+	 * Gets the index of the level in the levelpackage. 
+	 * @return the index of the level.
+	 */
 	public int getLevelIndex() {
 		return 0;
 	}
 
-	// LevelState state; //?
-	public Board getInputBoard() {
+	/**
+	 * Gets the board the level start with. 
+	 * @return the initial board.
+	 */
+	public Board getInitialBoard() {
 		return null;
 	}
 
+	/**
+	 * Gets the board, which has to be reached to win the level. 
+	 * @return the board which is the goal of the level.
+	 */
 	public Board getGoalBoard() {
 		return null;
 	}
 
-	public abstract boolean hasAnimation();
+	/**
+	 * Checks whether this level has a simulation or not. 
+	 * @return true if the level has a simulation otherwise false.
+	 */
+	public boolean hasAnimation();
 
+	/**
+	 * Gets the path to the animation of the level if it has one. 
+	 * @return the path to the animation of the level.
+	 */
 	public Animation getAnimation() {
 		return null;
+	}
+
+	/**
+	 * Gets the path to the hint of the level. 
+	 * @return the path to the hint of the level.
+	 */
+	public String gethint() {
+		return null;
+	}
+	
+	/**
+	 * Gets the description of the level. 
+	 * @return the description of the level.
+	 */
+	public String getDescription() {
+		return null;
+	}
+
+	/**
+	 * Gets the colors usable by the user. 
+	 * @return a array of colors.
+	 */
+	public Color[] getUserColors() {
+		return null;
+	}
+	
+	/**
+	 * Gets number of steps the simulation before it is aborted. 
+	 * @return the number of steps the simulation runs.
+	 */
+	public int getAbortSimulationAfter() {
+		return 0;
 	}
 }
