ce042a17b2dc0d41bb158c5a5e7e1348e0f0e8f7
Lena Winter <lena.k.winter@gmail.com>
2013-12-20 16:34:50 +0100

    Add references.

--- entwurf/java/de/croggle/game/visitor/FindEatingVisitor.java
+++ entwurf/java/de/croggle/game/visitor/FindEatingVisitor.java
@@ -2,9 +2,14 @@ package de.croggle.game.visitor;
 
 /**
  *
+ * @navassoc 1 - 1 de.croggle.game.event.BoardEventMessenger
  */
 public class FindEatingVisitor implements BoardObjectVisitor {
 	private boolean found;
+	private BoardEventMessenger boardMessenger;
+
+	public FindEatingVisitor(BoardEventMessenger boardMessenger){
+	}
 
 	/**
 	 *
