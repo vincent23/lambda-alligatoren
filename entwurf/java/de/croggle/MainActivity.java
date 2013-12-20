f0e8ffff6ad71989d52b2b21d24f3322966810d3
Iris M <iris.mehrbrodt@gmx.de>
2013-12-20 14:30:12 +0100

    Comment MainActivity

--- entwurf/java/de/croggle/MainActivity.java
+++ entwurf/java/de/croggle/MainActivity.java
@@ -1,10 +1,21 @@
 package de.croggle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import android.os.Bundle;
 
 /**
+ * Android backend that initializes the central ApplicationListener.
  * @depend - <creates> - AlligatorApp
  */
 public class MainActivity extends AndroidApplication {
+
+	/**
+	 * Initializes the central ApplicationListener. 
+	 * Is called by the android lifecycle as soon as the app is started. On return the inner app lifecycle of ApplicationListener is started.
+	 */
+	@Override
+	void onCreate(Bundle savedInstance){
+	
+	}
 
 }
