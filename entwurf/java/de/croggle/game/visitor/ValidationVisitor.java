c9bd1b77fc05e94c81508f77edb9169c6abe018b
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-21 20:43:33 +0100

    Add methods to ValidationVisitor

--- entwurf/java/de/croggle/game/visitor/ValidationVisitor.java
+++ entwurf/java/de/croggle/game/visitor/ValidationVisitor.java
@@ -4,15 +4,25 @@ import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
 import de.croggle.game.board.Board;
+import de.croggle.game.board.BoardObject;
 
 /**
- * A visitor checking whether the given Board represents a valid Lambda-Term (whether the evaluation is possilble or not).
+ * A visitor for checking whether the given Board represents a valid Lambda-Term (whether the evaluation is possilble or not).
  */
 public class ValidationVisitor implements BoardObjectVisitor {
 	/**
 	 *
 	 */
-	private ValidationVisitor() {
+	private ValidationVisitor(BoardObject family) {
+	}
+
+	/**
+	 * Checks whether the given family represents a valid LambdaTerm.
+	 *
+	 * @param family the family to check for validity
+	 * @return true if the family is valid, false otherwise
+	 */
+	public static boolean isValid(BoardObject family) {
 	}
 
 	/**
