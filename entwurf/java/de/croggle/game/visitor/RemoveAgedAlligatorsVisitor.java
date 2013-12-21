252317bf426440af884b8f0bfbbeeb78056eb3ad
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-21 20:39:42 +0100

    Add methods to RemoveOldAlligatorsVisitor

--- entwurf/java/de/croggle/game/visitor/RemoveAgedAlligatorsVisitor.java
+++ entwurf/java/de/croggle/game/visitor/RemoveAgedAlligatorsVisitor.java
@@ -4,11 +4,12 @@ import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
 import de.croggle.game.board.Board;
+import de.croggle.game.board.BoardObject;
 
 import de.croggle.game.event.BoardEventMessenger;
 
 /**
- * A visitor looking for aged alligators, which are not nessesary,
+ * A visitor looking for aged alligators, which are not nessesary
  * because their presence does not change the order of evaluation within the term.
  * @navassoc 1 - 1 de.croggle.game.event.BoardEventMessenger
  *
@@ -19,7 +20,16 @@ public class RemoveAgedAlligatorsVisitor implements BoardObjectVisitor {
 	/**
 	 *
 	 */
-	private RemoveAgedAlligatorsVisitor(BoardEventMessenger boardMessenger){
+	private RemoveAgedAlligatorsVisitor(BoardObject family, BoardEventMessenger boardMessenger){
+	}
+
+	/**
+	 * Removes all old alligators which are not necessary.
+	 *
+	 * @param family the family in which old alligators should be removed
+	 * @param boardMessenger the messenger used for notifying listeners about removed alligators
+	 */
+	public static void remove(BoardObject family, BoardEventMessenger boardMessenger) {
 	}
 
 	/**
