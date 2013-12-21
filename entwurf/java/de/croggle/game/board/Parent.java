0d1cf171d26359ea07e7f04a379ddd4f366beae5
Lena Winter <lena.k.winter@gmail.com>
2013-12-21 20:09:09 +0100

    Fix the javadocs in Board accordin to the javadoc conventions.

--- entwurf/java/de/croggle/game/board/Parent.java
+++ entwurf/java/de/croggle/game/board/Parent.java
@@ -15,15 +15,15 @@ public abstract class Parent {
 
 	/**
 	 * Adds a child to the familiy of the parent.
-	 * @param child is the InternalBoardObject, which is added to the family of the parent.
+	 * @param child the child is the InternalBoardObject, which is added to the family of the parent
 	 */
 	public void addChild(InternalBoardObject child) {
 	}
 
 	/**
 	 * Removes a child from the family of the parent.
-	 * @param child is the InternalBoardObject, which is removed from the family of the parent.
-	 * @return whether the removement was successfull.
+	 * @param child the child is the InternalBoardObject, which is removed from the family of the parent
+	 * @return whether the removement was successfull
 	 */
 	public boolean removeChild(InternalBoardObject child) {
 		return false;
@@ -37,7 +37,7 @@ public abstract class Parent {
 
 	/**
 	 * Returns an Iterator on the children list.
-	 * @return the Iterator.
+	 * @return the Iterator
 	 */
 	public Iterator<InternalBoardObject> getIterator() {
 		return null;
@@ -45,16 +45,16 @@ public abstract class Parent {
 
 	/**
 	 * Checks whether the given InternalBoardObject is the last child in the list of the parent.
-	 * @param child InternalChildObject which should be checked on.
-	 * @return returns true if the given child is the last child in the list false otherwise.
+	 * @param child the child is the InternalBoardObject which should be checked on
+	 * @return returns true if the given child is the last child in the list false otherwise
 	 */
 	public boolean isLastChild(InternalBoardObject child) {
 	}
 
 	/**
 	 * Returns the following child after the one given as parameter.
-	 * @param child in the children list.
-	 * @return the child which is the next after the child in the list.
+	 * @param child the child is an InternalBoardObject in the children list
+	 * @return the child which is the next after the child in the list
 	 */
 	public InternalBoardObject getNextChild(InternalBoardObject child) {
 	}
