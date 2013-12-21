b5c5e07b712321481e031a0e5df5557281502df4
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-21 20:24:34 +0100

    Add methods to CountBoardObjectsVisitor

--- entwurf/java/de/croggle/game/visitor/CountBoardObjectsVisitor.java
+++ entwurf/java/de/croggle/game/visitor/CountBoardObjectsVisitor.java
@@ -4,12 +4,28 @@ import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
 import de.croggle.game.board.Board;
+import de.croggle.game.board.BoardObject;
 
+/**
+ * A visitor for counting the number of objects in a family.
+ */
 public class CountBoardObjectsVisitor implements BoardObjectVisitor {
+	private int count;
+
 	/**
 	 *
 	 */
 	private CountBoardObjectsVisitor() {
+		this.count = 0;
+	}
+
+	/**
+	 * Count the number of objects in a family.
+	 *
+	 * @param family the family whose members should be counted
+	 * @return the number of family members
+	 */
+	public static int count(BoardObject family) {
 	}
 
 	/**
