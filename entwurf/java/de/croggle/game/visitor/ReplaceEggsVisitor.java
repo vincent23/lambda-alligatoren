9640e00defaaed324f8c6f886571d07a097e2895
Vincent Schüßler <v.schuessler@gmail.com>
2013-12-21 22:14:26 +0100

    Change signature of ReplaceEggsVisitor.replace()

--- entwurf/java/de/croggle/game/visitor/ReplaceEggsVisitor.java
+++ entwurf/java/de/croggle/game/visitor/ReplaceEggsVisitor.java
@@ -8,7 +8,6 @@ import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
 import de.croggle.game.board.Board;
-import de.croggle.game.board.BoardObject;
 
 /**
  * A visitor replacing eggs of a certain color with copies of a given
@@ -19,26 +18,19 @@ import de.croggle.game.board.BoardObject;
 public class ReplaceEggsVisitor implements BoardObjectVisitor {
 	private BoardEventMessenger boardMessenger;
 
-	/**
-	 * Creates a new visitor, which replaces eggs of the color <code>eggColor</code> with copies of <code>bornFamily</code>.
-	 *
-	 * @param eggColor the color of the eggs to replace
-	 * @param bornFamily the family with which eggs are replaced
-	 * @param boardMessenger the messenger used for sending events when eggs are replaced
-	 */
-	private ReplaceEggsVisitor(Color eggColor, InternalBoardObject bornFamily, BoardEventMessenger boardMessenger){
+	private ReplaceEggsVisitor(ColoredAlligator parent, InternalBoardObject bornFamily, BoardEventMessenger boardMessenger){
 	}
 
 	/**
-	 * Replaces all egs of the color <code>eggColor</code> in the <code>family</code> with copies of <code>bornFamily</code>.
+	 * Replaces all eggs below <code>parent</code>, which share it's color, with a copy of <code>bornFamily</code>.
+	 * Correct recoloring of the newly inserted families is also performed, if necessary.
 	 * When an egg is replaced, an onEat event is sent through <code>boardMessenger</code>.
 	 *
-	 * @param family the family in which eggs should be replaced
-	 * @param eggColor the color of the eggs to replace
+	 * @param parent the colored alligator whose child eggs should be replaced
 	 * @param bornFamily the family with which eggs are replaced
 	 * @param boardMessenger the messenger used for sending events when eggs are replaced
 	 */
-	public static void replace(BoardObject family, Color eggColor, InternalBoardObject bornFamily, BoardEventMessenger boardMessenger) {
+	public static void replace(ColoredAlligator parent, InternalBoardObject bornFamily, BoardEventMessenger boardMessenger) {
 	}
 
 	/**
