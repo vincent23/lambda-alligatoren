6cc97e31e6c111f514493c1887f6b82ca17ca050
Iris M <iris.mehrbrodt@gmx.de>
2013-12-22 13:24:52 +0100

    Add SplashScreen

---
+++ entwurf/java/de/croggle/ui/screens/SplashScreen.java
@@ -0,0 +1,17 @@
+package de.croggle.ui.screens;
+
+/**
+ * The screen that is shown when the app is started freshly. Will show the app
+ * logo and name or something.
+ * 
+ */
+public class SplashScreen extends AbstractScreen {
+
+	/**
+	 * Creates the empty splash screen.
+	 */
+	public SplashScreen() {
+
+	}
+
+}
