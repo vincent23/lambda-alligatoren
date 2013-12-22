c0c8dc852650c0999708fb9f7bb7217d38d0f0a3
Tobias Hornberger <saibot1207@googlemail.com>
2013-12-22 18:21:16 +0100

    add Javadoc

--- entwurf/java/de/croggle/ui/renderer/BoardObjectActor.java
+++ entwurf/java/de/croggle/ui/renderer/BoardObjectActor.java
@@ -3,8 +3,8 @@ package de.croggle.ui.renderer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
-/*
- * 
+/**
+ * An actor used for representing a board object.
  */
 public abstract class BoardObjectActor extends Actor {
 }
