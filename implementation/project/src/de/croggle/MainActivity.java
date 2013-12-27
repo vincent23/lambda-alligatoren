df0fc96b9b368c8e331ebbf7d601940145a82c51
Lukas Boehm <suluke93@gmail.com>
2013-12-27 12:48:27 +0100

    Change options with which the libgdx AlligatorApp game is instantiated

--- implementation/project/src/de/croggle/MainActivity.java
+++ implementation/project/src/de/croggle/MainActivity.java
@@ -3,6 +3,7 @@ package de.croggle;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 /**
  * Android backend that initializes the central ApplicationListener.
@@ -13,11 +14,21 @@ public class MainActivity extends AndroidApplication {
 	 * Initializes the central ApplicationListener. Is called by the android
 	 * lifecycle as soon as the app is started. On return, the inner app
 	 * lifecycle of ApplicationListener is started.
+	 * 
+	 * @param savedInstanceState  a Bundle containing the activity's previously frozen state, if there was one.
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(new AlligatorApp(this), true);
+		
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		
+		config.useGL20 = true;
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useWakelock = true;
+		
+		initialize(new AlligatorApp(this), config);
 	}
 
 }
