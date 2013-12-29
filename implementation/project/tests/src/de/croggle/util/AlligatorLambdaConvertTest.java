6f730f94d1ee653644d165db3ec91a2c898d147e
Lukas Boehm <suluke93@gmail.com>
2013-12-29 16:31:33 +0100

    Move test project according to http://developer.android.com/tools/testing/testing_android.html: 'the best approach is to add the test project so that its root directory tests/ is at the same level as the src/'

---
+++ implementation/project/tests/src/de/croggle/util/AlligatorLambdaConvertTest.java
@@ -0,0 +1,23 @@
+package de.croggle.util;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class AlligatorLambdaConvertTest extends TestCase {
+
+	public AlligatorLambdaConvertTest() {
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		
+	}
+
+
+	@Override
+	public void tearDown() {
+	}
+
+}
