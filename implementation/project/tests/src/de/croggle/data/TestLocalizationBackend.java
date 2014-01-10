e660dc517d07e0d4b408d370718bf5853afb67e0
Lukas Boehm <suluke93@gmail.com>
2014-01-10 11:08:22 +0100

    Add TestLocalizationBackend

---
+++ implementation/project/tests/src/de/croggle/data/TestLocalizationBackend.java
@@ -0,0 +1,64 @@
+package de.croggle.data;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class TestLocalizationBackend implements LocalizationBackend {
+
+	private Locale c;
+	
+	private Map<String, String[]> arrays;
+	private Map<String, String> strings;
+	private Map<String, Map<Integer, String>> quantities;
+	
+	public TestLocalizationBackend() {
+		this.c = Locale.getDefault();
+		arrays = new HashMap<String, String[]>();
+		strings = new HashMap<String, String>();
+		quantities = new HashMap<String, Map<Integer,String>>();
+	}
+	
+	public void putString(String key, String val) {
+		strings.put(key, val);
+	}
+	
+	public void putArray(String key, String[] val) {
+		arrays.put(key, val);
+	}
+	
+	public void putQuantities(String key, Map<Integer, String> val) {
+		quantities.put(key, val);
+	}
+	
+	@Override
+	public String translate(String s) {
+		return strings.get(s);
+	}
+
+	@Override
+	public String translate(String s, int multiplicity) {
+		return quantities.get(s).get(multiplicity);
+	}
+
+	@Override
+	public String[] getLocalizedStringList(String identifier) {
+		return arrays.get(identifier);
+	}
+
+	@Override
+	public void setApplicationLocale(Locale locale) {
+		this.c = locale;
+	}
+
+	@Override
+	public Locale getApplicationLocale() {
+		return c;
+	}
+
+	@Override
+	public Locale getSystemLocale() {
+		return Locale.getDefault();
+	}
+
+}
