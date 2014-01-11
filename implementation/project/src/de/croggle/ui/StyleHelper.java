1e449a36f3651a919a67e980243dd113630190b5
Iris M <iris.mehrbrodt@gmx.de>
2014-01-11 20:09:30 +0100

    Add some ui files to work with

--- implementation/project/src/de/croggle/ui/StyleHelper.java
+++ implementation/project/src/de/croggle/ui/StyleHelper.java
@@ -1,19 +1,30 @@
 package de.croggle.ui;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton.ImageButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton.ImageTextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
+import de.croggle.data.AssetManager;
+
 /**
  * Singleton to manage Styles.
  **/
 public class StyleHelper {
 
+	private static StyleHelper instance;
 	private Skin skin;
 
-	private StyleHelper() {
+	public StyleHelper() {
+		AssetManager manager = AssetManager.getInstance();
+		manager.load("textures/pack.atlas", TextureAtlas.class);
+		manager.finishLoading();
+		skin = new Skin(Gdx.files.internal("skin.json"), manager.get(
+				"textures/pack.atlas", TextureAtlas.class));
 	}
 
 	/**
@@ -21,8 +32,11 @@ public class StyleHelper {
 	 * 
 	 * @return the current instance
 	 */
-	public StyleHelper getInstance() {
-		return null;
+	public static StyleHelper getInstance() {
+		if (instance == null) {
+			instance = new StyleHelper();
+		}
+		return instance;
 	}
 
 	/**
@@ -31,7 +45,16 @@ public class StyleHelper {
 	 * @return the skin
 	 */
 	public Skin getSkin() {
-		return null;
+		return skin;
+	}
+
+	/**
+	 * Gets the default button style.
+	 * 
+	 * @return
+	 */
+	public ButtonStyle getButtonStyle() {
+		return skin.get(ButtonStyle.class);
 	}
 
 	/**
@@ -39,8 +62,8 @@ public class StyleHelper {
 	 * 
 	 * @return the text button's style
 	 */
-	TextButtonStyle getTextButtonStyle() {
-		return null;
+	public TextButtonStyle getTextButtonStyle() {
+		return skin.get(TextButtonStyle.class);
 	}
 
 	/**
@@ -57,7 +80,7 @@ public class StyleHelper {
 	 * 
 	 * @return the image text button's style
 	 */
-	ImageTextButtonStyle getImageTextButtonStyle() {
+	public ImageTextButtonStyle getImageTextButtonStyle() {
 		return null;
 	}
 
@@ -68,13 +91,6 @@ public class StyleHelper {
 	 */
 	LabelStyle getLabelStyle() {
 		return null;
-	}
-
-	/**
-	 * Inizializes the style helper, so it can be used by the screens.
-	 */
-	private void initialize() {
-
 	}
 
 }
--- implementation/project/src/de/croggle/ui/actors/ProfileButton.java
+++ implementation/project/src/de/croggle/ui/actors/ProfileButton.java
@@ -1,15 +1,18 @@
 package de.croggle.ui.actors;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import de.croggle.game.profile.Profile;
+import de.croggle.ui.StyleHelper;
 
 /**
  * An actor for the main menu's profile button. Shows the currently selected
  * profile's name and avatar picture.
  * 
  */
-public class ProfileButton extends Actor {
+public class ProfileButton extends Button {
 	private final Profile user;
 
 	/**
@@ -20,6 +23,11 @@ public class ProfileButton extends Actor {
 	 *            the user profile to display as currently active
 	 */
 	public ProfileButton(Profile user) {
+		super(StyleHelper.getInstance().getButtonStyle());
 		this.user = user;
+
+		add(user.getName()).row();
+		add(new Image(new Texture(user.getPicturePath())));
+
 	}
 }
