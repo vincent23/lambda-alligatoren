fcf3c675ad4e1386b4f4fcd83823ced0325768dd
Vincent Schüßler <v.schuessler@gmail.com>
2014-01-12 17:34:08 +0100

    Add more tests to CollectFreeColorsTest

--- implementation/project/tests/src/de/croggle/game/board/operations/CollectFreeColorsTest.java
+++ implementation/project/tests/src/de/croggle/game/board/operations/CollectFreeColorsTest.java
@@ -56,4 +56,62 @@ public class CollectFreeColorsTest extends CollectColorsTest {
 		Assert.assertTrue(arrayContainsColor(freeColors, color1));
 		Assert.assertFalse(arrayContainsColor(freeColors, color2));
 	}
+
+	public void testMultipleFreeColors() {
+		final Board board = new Board();
+		final Color color1 = new Color(1);
+		final Color color2 = new Color(2);
+		final Color color3 = new Color(3);
+		final Egg egg1 = new Egg(false, false, color1, false);
+		final Egg egg2 = new Egg(false, false, color2, false);
+		final Egg egg3 = new Egg(false, false, color3, false);
+		final ColoredAlligator colored1 = new ColoredAlligator(false, false,
+				color2, false);
+		final ColoredAlligator colored2 = new ColoredAlligator(false, false,
+				color1, false);
+		final ColoredAlligator colored3 = new ColoredAlligator(false, false,
+				color3, false);
+
+		board.addChild(colored1);
+		board.addChild(colored2);
+		board.addChild(colored3);
+		colored1.addChild(egg1);
+		colored2.addChild(egg2);
+		colored3.addChild(egg3);
+
+		final Color[] freeColors = CollectFreeColors.collect(board);
+		Assert.assertEquals(2, freeColors.length);
+		Assert.assertTrue(arrayContainsColor(freeColors, color1));
+		Assert.assertTrue(arrayContainsColor(freeColors, color2));
+		Assert.assertFalse(arrayContainsColor(freeColors, color3));
+	}
+
+	public void testFreeColorMultipleOccurences() {
+		final Board board = new Board();
+		final Color color1 = new Color(1);
+		final Color color2 = new Color(2);
+		final Egg egg1 = new Egg(false, false, color1, false);
+		final Egg egg2 = new Egg(false, false, color1, false);
+		final Egg egg3 = new Egg(false, false, color1, false);
+		final Egg egg4 = new Egg(false, false, color2, false);
+		final ColoredAlligator colored1 = new ColoredAlligator(false, false,
+				color2, false);
+		final ColoredAlligator colored2 = new ColoredAlligator(false, false,
+				color1, false);
+		final ColoredAlligator colored3 = new ColoredAlligator(false, false,
+				color1, false);
+
+		board.addChild(colored1);
+		board.addChild(colored2);
+		board.addChild(colored3);
+		colored1.addChild(egg1);
+		colored1.addChild(egg2);
+		colored2.addChild(egg3);
+		colored3.addChild(egg4);
+
+		final Color[] freeColors = CollectFreeColors.collect(board);
+		Assert.assertEquals(2, freeColors.length);
+		Assert.assertTrue(arrayContainsColor(freeColors, color1));
+		Assert.assertTrue(arrayContainsColor(freeColors, color2));
+	}
 }
