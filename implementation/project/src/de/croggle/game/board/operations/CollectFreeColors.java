5236e2821d41c02a10e04e3f0ad8722cff94c6e5
Vincent Schüßler <v.schuessler@gmail.com>
2014-01-12 14:29:35 +0100

    Implement CollectFreeColors operation

--- implementation/project/src/de/croggle/game/board/operations/CollectFreeColors.java
+++ implementation/project/src/de/croggle/game/board/operations/CollectFreeColors.java
@@ -1,21 +1,29 @@
 package de.croggle.game.board.operations;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import de.croggle.game.Color;
 import de.croggle.game.board.AgedAlligator;
 import de.croggle.game.board.Board;
 import de.croggle.game.board.BoardObject;
 import de.croggle.game.board.ColoredAlligator;
 import de.croggle.game.board.Egg;
+import de.croggle.game.board.Parent;
 
 /**
  * A visitor for collecting all the colors of eggs with no matching alligator
  * above them. This is equivalent to the set of variables which occur free in a
  * given subterm.
  * 
- * TODO implement
  */
 public class CollectFreeColors implements BoardObjectVisitor {
+	private final Set<Color> freeColors;
+	private final BoardObject family;
+
 	private CollectFreeColors(BoardObject family) {
+		freeColors = new HashSet<Color>();
+		this.family = family;
 	}
 
 	/**
@@ -27,7 +35,9 @@ public class CollectFreeColors implements BoardObjectVisitor {
 	 * @return the set of free colors
 	 */
 	public static Color[] collect(BoardObject family) {
-		return null;
+		final CollectFreeColors visitor = new CollectFreeColors(family);
+		family.accept(visitor);
+		return (Color[]) visitor.freeColors.toArray();
 	}
 
 	/**
@@ -35,7 +45,18 @@ public class CollectFreeColors implements BoardObjectVisitor {
 	 */
 	@Override
 	public void visitEgg(Egg egg) {
-
+		final Color color = egg.getColor();
+		Parent parent;
+		do {
+			parent = egg.getParent();
+			if (parent instanceof ColoredAlligator) {
+				final ColoredAlligator colored = (ColoredAlligator) parent;
+				if (color.equals(colored.getColor())) {
+					return;
+				}
+			}
+		} while (parent != family);
+		freeColors.add(color);
 	}
 
 	/**
@@ -43,7 +64,7 @@ public class CollectFreeColors implements BoardObjectVisitor {
 	 */
 	@Override
 	public void visitColoredAlligator(ColoredAlligator alligator) {
-
+		alligator.acceptOnChildren(this);
 	}
 
 	/**
@@ -51,7 +72,7 @@ public class CollectFreeColors implements BoardObjectVisitor {
 	 */
 	@Override
 	public void visitAgedAlligator(AgedAlligator alligator) {
-
+		alligator.acceptOnChildren(this);
 	}
 
 	/**
@@ -59,6 +80,6 @@ public class CollectFreeColors implements BoardObjectVisitor {
 	 */
 	@Override
 	public void visitBoard(Board board) {
-
+		board.acceptOnChildren(this);
 	}
 }
