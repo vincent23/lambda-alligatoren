f8a2a2c9f34db3d6b6055ca2a935f6dd4e332015
Lukas Boehm <suluke93@gmail.com>
2014-01-18 18:23:42 +0100

    Move color-from-string methods to dedicated helper class in croggle.util

--- implementation/project/src/de/croggle/game/ColorController.java
+++ implementation/project/src/de/croggle/game/ColorController.java
@@ -102,7 +102,7 @@ public class ColorController {
 		}
 
 		de.croggle.game.Color c = new de.croggle.game.Color(lookup.size());
-		this.lookup.put(c, colorFromHexString(colors[c.getId()]));
+		this.lookup.put(c, de.croggle.util.convert.Color.fromHexString(colors[c.getId()]));
 		return c;
 	}
 
@@ -133,7 +133,7 @@ public class ColorController {
 
 		de.croggle.game.Color c = new de.croggle.game.Color(i);
 		if (this.lookup.size() <= i) {
-			this.lookup.put(c, colorFromHexString(colors[i]));
+			this.lookup.put(c, de.croggle.util.convert.Color.fromHexString(colors[i]));
 		}
 
 		return c;
@@ -190,51 +190,5 @@ public class ColorController {
 	 */
 	public boolean isUsable(de.croggle.game.Color color) {
 		return this.usableColors.contains(color);
-	}
-
-	/*
-	 * Expects a hex value as integer and returns the appropriate Color object.
-	 * 
-	 * @param hex must be of the form 0xAARRGGBB
-	 * 
-	 * @return the generated Color object
-	 * 
-	 * @author Michael.Lowfyr@gmail.com -
-	 * https://code.google.com/p/libgdx-users/wiki/ColorHexConverter
-	 */
-	private com.badlogic.gdx.graphics.Color colorFromHex(long hex) {
-		float a = (hex & 0xFF000000L) >> 24;
-		float r = (hex & 0xFF0000L) >> 16;
-		float g = (hex & 0xFF00L) >> 8;
-		float b = (hex & 0xFFL);
-
-		return new com.badlogic.gdx.graphics.Color(r / 255f, g / 255f,
-				b / 255f, a / 255f);
-	}
-
-	/*
-	 * Expects a hex value as String and returns the appropriate Color object
-	 * 
-	 * @param s the hex string to create the Color object from
-	 * 
-	 * @return the generated Color object
-	 * 
-	 * @author Michael.Lowfyr@gmail.com -
-	 * https://code.google.com/p/libgdx-users/wiki/ColorHexConverter
-	 */
-	private com.badlogic.gdx.graphics.Color colorFromHexString(String s) {
-		if (s.startsWith("0x"))
-			s = s.substring(2);
-		else if (s.startsWith("#"))
-			s = s.substring(1);
-
-		if (s.length() == 6)
-			s = "FF" + s;
-
-		if (s.length() != 8) // AARRGGBB
-			throw new IllegalArgumentException(
-					"String must have the form AARRGGBB");
-
-		return colorFromHex(Long.parseLong(s, 16));
 	}
 }
---
+++ implementation/project/src/de/croggle/util/convert/Color.java
@@ -0,0 +1,53 @@
+package de.croggle.util.convert;
+
+public class Color {
+	private Color() {
+		
+	}
+	
+	/*
+	 * Expects a hex value as integer and returns the appropriate Color object.
+	 * 
+	 * @param hex must be of the form 0xAARRGGBB
+	 * 
+	 * @return the generated Color object
+	 * 
+	 * @author Michael.Lowfyr@gmail.com -
+	 * https://code.google.com/p/libgdx-users/wiki/ColorHexConverter
+	 */
+	public static com.badlogic.gdx.graphics.Color fromHex(long hex) {
+		float a = (hex & 0xFF000000L) >> 24;
+		float r = (hex & 0xFF0000L) >> 16;
+		float g = (hex & 0xFF00L) >> 8;
+		float b = (hex & 0xFFL);
+
+		return new com.badlogic.gdx.graphics.Color(r / 255f, g / 255f,
+				b / 255f, a / 255f);
+	}
+
+	/*
+	 * Expects a hex value as String and returns the appropriate Color object
+	 * 
+	 * @param s the hex string to create the Color object from
+	 * 
+	 * @return the generated Color object
+	 * 
+	 * @author Michael.Lowfyr@gmail.com -
+	 * https://code.google.com/p/libgdx-users/wiki/ColorHexConverter
+	 */
+	public static com.badlogic.gdx.graphics.Color fromHexString(String s) {
+		if (s.startsWith("0x"))
+			s = s.substring(2);
+		else if (s.startsWith("#"))
+			s = s.substring(1);
+
+		if (s.length() == 6)
+			s = "FF" + s;
+
+		if (s.length() != 8) // AARRGGBB
+			throw new IllegalArgumentException(
+					"String must have the form AARRGGBB");
+
+		return fromHex(Long.parseLong(s, 16));
+	}
+}
