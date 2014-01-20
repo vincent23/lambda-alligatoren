44ff015268bb1b35f305665569fb8ea188490a78
Iris M <iris.mehrbrodt@gmx.de>
2014-01-20 23:16:57 +0100

    Layout LevelTerminatedScreen

--- implementation/project/src/de/croggle/ui/screens/LevelTerminatedScreen.java
+++ implementation/project/src/de/croggle/ui/screens/LevelTerminatedScreen.java
@@ -1,13 +1,22 @@
 package de.croggle.ui.screens;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+
 import de.croggle.AlligatorApp;
 import de.croggle.game.GameController;
+import de.croggle.game.achievement.AchievementController;
+import de.croggle.ui.StyleHelper;
 
 /**
  * First screen seen after completing a level. For reference see ``Pflichtenheft
  * 10.5.6 / Abbildung 15''.
  */
 public class LevelTerminatedScreen extends AbstractScreen {
+
+	private GameController gameController;
+	private AchievementController achievementController;
+
 	/**
 	 * Creates the level terminated screen that is shown to the player after the
 	 * completion of a level.
@@ -20,5 +29,31 @@ public class LevelTerminatedScreen extends AbstractScreen {
 	 */
 	public LevelTerminatedScreen(AlligatorApp game, GameController controller) {
 		super(game);
+		gameController = controller;
+		achievementController = game.getAchievementController();
+
+		fillTable();
+	}
+
+	private void fillTable() {
+		StyleHelper helper = StyleHelper.getInstance();
+
+		Image image = new Image(helper.getDrawable("widgets/icon-play"));
+		ImageButton next = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton levelOverview = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton replay = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton achievements = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+
+		table.pad(30);
+		table.add(image).colspan(4).expand();
+		table.row();
+		table.add(achievements).expandX().left().size(150);
+		table.add(replay).size(100).bottom().space(30);
+		table.add(levelOverview).size(100).bottom().space(30);
+		table.add(next).size(150);
 	}
 }
