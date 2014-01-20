50eca8364a450edcbc032f89ebd9a0ea34e00e59
Iris M <iris.mehrbrodt@gmx.de>
2014-01-20 22:31:03 +0100

    Add dialog for achievement notifications

---
+++ implementation/project/src/de/croggle/ui/screens/NewAchievementDialog.java
@@ -0,0 +1,48 @@
+package de.croggle.ui.screens;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import de.croggle.game.achievement.Achievement;
+import de.croggle.ui.StyleHelper;
+
+public class NewAchievementDialog extends Dialog {
+
+	public NewAchievementDialog(Achievement achievement, int index,
+			boolean hasCloseButton) {
+		super("", StyleHelper.getInstance().getDialogStyle());
+
+		StyleHelper helper = StyleHelper.getInstance();
+		Label message = new Label("You got a new achievement!",
+				helper.getLabelStyle());
+		Image icon = new Image(helper.getDrawable(achievement
+				.getEmblemPath(index)));
+		Label description = new Label(achievement.getDescription(index),
+				helper.getLabelStyle());
+		ImageButton next = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+
+		add(message).row();
+		add(icon).row();
+		add(description).row();
+
+		if (hasCloseButton) {
+			add(next);
+			next.addListener(new DisposeListener());
+		} else {
+			this.addListener(new DisposeListener());
+		}
+	}
+
+	private class DisposeListener extends ClickListener {
+		@Override
+		public void clicked(InputEvent event, float x, float y) {
+			NewAchievementDialog.this.cancel();
+		}
+	}
+
+}
