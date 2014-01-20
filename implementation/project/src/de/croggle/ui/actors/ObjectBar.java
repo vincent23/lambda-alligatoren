b3ab74db4e7c21131f8bda9d3431162713d92836
Iris M <iris.mehrbrodt@gmx.de>
2014-01-20 21:57:49 +0100

    Layout PlacementModeScreen

--- implementation/project/src/de/croggle/ui/actors/ObjectBar.java
+++ implementation/project/src/de/croggle/ui/actors/ObjectBar.java
@@ -1,7 +1,10 @@
 package de.croggle.ui.actors;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
+import de.croggle.game.board.AgedAlligator;
+import de.croggle.ui.StyleHelper;
 import de.croggle.ui.renderer.AgedAlligatorActor;
 import de.croggle.ui.renderer.ColoredAlligatorActor;
 import de.croggle.ui.renderer.EggActor;
@@ -9,7 +12,7 @@ import de.croggle.ui.renderer.EggActor;
 /**
  * The bar to drag alligators and eggs from onto the screen.
  **/
-public class ObjectBar extends Actor {
+public class ObjectBar extends Table {
 
 	private AgedAlligatorActor agedAlligator;
 	private ColoredAlligatorActor coloredAlligator;
@@ -20,6 +23,23 @@ public class ObjectBar extends Actor {
 	 * screen per default.
 	 */
 	public ObjectBar() {
+		agedAlligator = new AgedAlligatorActor(new AgedAlligator(true, false));
+		// coloredAlligator = new ColoredAlligatorActor(new
+		// ColoredAlligator(true,
+		// false, new Color(1), false), new ColorController());
+		// egg = new EggActor(new Egg(true, false, new Color(1), false),
+		// new ColorController());
+
+		ImageButton startSimulation = new ImageButton(StyleHelper.getInstance()
+				.getImageButtonStyleRound("widgets/dummy-button"));
+
+		setBackground(StyleHelper.getInstance().getDrawable(
+				"widgets/dummy-button"));
+
+		add(coloredAlligator).row();
+		// add(agedAlligator).row();
+		// add(egg).row();
+		add(startSimulation);
 	}
 
 }
--- implementation/project/src/de/croggle/ui/screens/PlacementModeScreen.java
+++ implementation/project/src/de/croggle/ui/screens/PlacementModeScreen.java
@@ -1,13 +1,20 @@
 package de.croggle.ui.screens;
 
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 import de.croggle.AlligatorApp;
 import de.croggle.game.GameController;
+import de.croggle.ui.StyleHelper;
+import de.croggle.ui.actors.ObjectBar;
 
 /**
  * Screen within which the player can manipulate the board by moving alligators
  * and eggs. For reference see ``Pflichtenheft 10.5.4 / Abbildungen 12 und 1''.
  */
 public class PlacementModeScreen extends AbstractScreen {
+
+	private GameController gameController;
 
 	/**
 	 * Creates the screen of a level within the placement mode. This is the
@@ -21,5 +28,38 @@ public class PlacementModeScreen extends AbstractScreen {
 	 */
 	public PlacementModeScreen(AlligatorApp game, GameController controller) {
 		super(game);
+		gameController = controller;
+
+		fillTable();
+	}
+
+	private void fillTable() {
+		StyleHelper helper = StyleHelper.getInstance();
+
+		Table leftTable = new Table();
+		ImageButton menu = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton hint = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton zoomIn = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ImageButton zoomOut = new ImageButton(
+				helper.getImageButtonStyleRound("widgets/dummy-icon"));
+		ObjectBar objectBar = new ObjectBar();
+
+		leftTable.pad(30);
+		leftTable.defaults().space(30);
+		leftTable.add(menu).size(100).top().left();
+		leftTable.row();
+		// TODO only activated after some time
+		leftTable.add(hint).expand().size(100).top().left();
+		leftTable.row();
+		// TODO only if zoom buttons are enabled
+		leftTable.add(zoomIn).size(50).left();
+		leftTable.row();
+		leftTable.add(zoomOut).size(50).left();
+
+		table.add(leftTable).expand().fill();
+		table.add(objectBar).padLeft(30);
 	}
 }
