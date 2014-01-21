110aadc7f07fb1a746f104189a0e89ff3e79bdb3
Tobias Hornberger <saibot1207@googlemail.com>
2014-01-21 12:33:05 +0100

    Add test for LevelAchievement

---
+++ implementation/project/tests/src/de/croggle/game/achievement/LevelAchievementTest.java
@@ -0,0 +1,42 @@
+package de.croggle.game.achievement;
+
+import de.croggle.data.LocalizationHelper;
+import de.croggle.data.TestLocalizationBackend;
+import de.croggle.data.persistence.Statistic;
+import junit.framework.TestCase;
+
+public class LevelAchievementTest extends TestCase {
+
+	public void testInitialize() {
+		TestLocalizationBackend backend = new TestLocalizationBackend();
+		LocalizationHelper.setBackend(backend);
+		backend.putString("achievement_level_completed", " Levels completed");
+		Achievement testAchievement = new LevelAchievement();
+		testAchievement.initialize();
+		assertTrue(testAchievement.getId() == 0);
+		int[] testStages = { 0, 1, 2, 4, 6, 8, 10, 12 };
+		for (int i = 1; i < 8; i++) {
+			assertTrue(testAchievement.getStage(i) == testStages[i]);
+			assertTrue(testAchievement.getDescription(i).endsWith("completed"));
+			assertTrue(testAchievement.getEmblemPath(i).equals("emblems/levelCompleted/0" + i));
+			
+		}
+	} 
+	
+	public void testIndexingAndIncreaseDecrease() {
+		Achievement testAchievement = new LevelAchievement();
+		int[] testStages = { 0, 1, 2, 4, 6, 8, 10, 12 };
+		testAchievement.setStages(testStages);
+		Statistic testStatistic = new Statistic();
+		testStatistic.setLevelsComplete(14);
+		assertTrue(testAchievement.requirementsMet(testStatistic, testStatistic) == 7);
+		testStatistic.setLevelsComplete(2);
+		testAchievement.setIndex(0);
+		assertTrue(testAchievement.requirementsMet(testStatistic, testStatistic) == 2);
+		testStatistic.setLevelsComplete(0);
+		testAchievement.setIndex(0);
+		assertTrue(testAchievement.requirementsMet(testStatistic, testStatistic) == 0);
+		
+		
+	}
+}
