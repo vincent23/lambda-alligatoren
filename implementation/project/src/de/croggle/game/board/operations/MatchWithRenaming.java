2ddcc1870734e5cdc206141117e2560b95c03403
Lukas Boehm <suluke93@gmail.com>
2014-01-22 12:19:48 +0100

    Implement MatchWithRenaming and add simple test case

--- implementation/project/src/de/croggle/game/board/operations/MatchWithRenaming.java
+++ implementation/project/src/de/croggle/game/board/operations/MatchWithRenaming.java
@@ -1,6 +1,16 @@
 package de.croggle.game.board.operations;
 
+import java.util.Iterator;
+
+import android.util.SparseArray;
+import de.croggle.game.Color;
+import de.croggle.game.board.AgedAlligator;
+import de.croggle.game.board.Board;
 import de.croggle.game.board.BoardObject;
+import de.croggle.game.board.ColoredAlligator;
+import de.croggle.game.board.Egg;
+import de.croggle.game.board.InternalBoardObject;
+import de.croggle.game.board.Parent;
 
 /**
  * A board operation that offers functionality to compare alligator expressions
@@ -10,9 +20,15 @@ import de.croggle.game.board.BoardObject;
  * of expression b exists (alpha equivalence).
  * 
  */
-public class MatchWithRenaming {
-	private MatchWithRenaming() {
+public class MatchWithRenaming implements BoardObjectVisitor {
 
+	private boolean match = true;
+	private BoardObject toCompare;
+	private SparseArray<Color> mapping;
+
+	private MatchWithRenaming(BoardObject toCompare) {
+		this.toCompare = toCompare;
+		mapping = new SparseArray<Color>();
 	}
 
 	/**
@@ -27,7 +43,75 @@ public class MatchWithRenaming {
 	 *         otherwise
 	 */
 	public static boolean match(BoardObject a, BoardObject b) {
-		throw new UnsupportedOperationException("Not implemented yet");
+		MatchWithRenaming matcher = new MatchWithRenaming(b);
+		a.accept(matcher);
+		return matcher.match;
+	}
+
+	private boolean colorsMatch(Color one, Color two) {
+		if (mapping.get(one.getId()) == null) {
+			mapping.append(one.getId(), two);
+			return true;
+		} else {
+			return mapping.get(one.getId()).equals(two);
+		}
+	}
+
+	@Override
+	public void visitEgg(Egg egg) {
+		if (toCompare.getClass() == Egg.class) {
+			if (colorsMatch(egg.getColor(), ((Egg) toCompare).getColor())) {
+				return;
+			}
+		}
+		match = false;
+	}
+
+	@Override
+	public void visitColoredAlligator(ColoredAlligator alligator) {
+		if (toCompare.getClass() == ColoredAlligator.class) {
+			if (colorsMatch(alligator.getColor(),
+					((ColoredAlligator) toCompare).getColor())) {
+				visitParent(alligator, (ColoredAlligator) toCompare);
+				return;
+			}
+		}
+		match = false;
+	}
+
+	@Override
+	public void visitAgedAlligator(AgedAlligator alligator) {
+		if (toCompare.getClass() == AgedAlligator.class) {
+			visitParent(alligator, (AgedAlligator) toCompare);
+		} else {
+			match = false;
+		}
+	}
+
+	@Override
+	public void visitBoard(Board board) {
+		if (toCompare.getClass() == Board.class) {
+			visitParent(board, (Board) toCompare);
+		} else {
+			match = false;
+		}
+	}
+
+	private void visitParent(Parent thisParent, Parent compareParent) {
+		if (thisParent.getChildCount() == compareParent.getChildCount()) {
+			// just to make sure there is no inconsistency if someone executes code
+			// after calling visitParent
+			BoardObject toCmp = toCompare;
+			Iterator<InternalBoardObject> thisIt = thisParent.iterator();
+			Iterator<InternalBoardObject> compareIt = compareParent.iterator();
+			while (match && thisIt.hasNext()) {
+				toCompare = compareIt.next();
+				thisIt.next().accept(this);
+			}
+			toCompare = toCmp;
+		} else {
+			match = false;
+		}
 	}
 
 }
