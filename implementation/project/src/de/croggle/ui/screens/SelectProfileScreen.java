bbe124b2de4b7f8f440989346ec1f7c0fa51c6f8
Iris M <iris.mehrbrodt@gmx.de>
2014-01-24 00:11:49 +0100

    Fix and restructure SelectProfileScreen

--- implementation/project/src/de/croggle/ui/screens/SelectProfileScreen.java
+++ implementation/project/src/de/croggle/ui/screens/SelectProfileScreen.java
@@ -1,10 +1,10 @@
 package de.croggle.ui.screens;
 
-
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import de.croggle.AlligatorApp;
@@ -17,7 +17,8 @@ import de.croggle.ui.StyleHelper;
  * Screen within which the player can change the name of his profile. For
  * reference see ``Pflichtenheft 10.5.12 / Abbildung 21''.
  */
-public class SelectProfileScreen extends AbstractScreen implements OnProfileChangeListener{
+public class SelectProfileScreen extends AbstractScreen implements
+		OnProfileChangeListener {
 
 	private ProfileController profileController;
 
@@ -27,7 +28,6 @@ public class SelectProfileScreen extends AbstractScreen implements OnProfileChan
 	 * 
 	 * @param game
 	 *            the backreference to the central game
-
 	 */
 	public SelectProfileScreen(AlligatorApp game) {
 		super(game);
@@ -38,38 +38,42 @@ public class SelectProfileScreen extends AbstractScreen implements OnProfileChan
 
 	private void fillTable() {
 		StyleHelper helper = StyleHelper.getInstance();
+
+		Table scrollTable = new Table();
 		ImageButton newProfile = new ImageButton(
-				helper.getImageButtonStyle("widgets/dummy-icon"));
+				helper.getImageButtonStyle("widgets/icon-plus"));
+
 		newProfile.addListener(new ProfileSetNameScreenClickListener());
-		table.add(newProfile).uniform();
+
+		scrollTable.defaults().width(500).height(100).space(10);
+
+		for (Profile profile : profileController.getAllProfiles()) {
+			TextButton profileButton = new TextButton(profile.getName(),
+					helper.getTextButtonStyle());
+			scrollTable.add(profileButton);
+			scrollTable.row();
+			profileButton.addListener(new ChangeProfileClickListener());
+		}
+
+		scrollTable.add(newProfile);
+
+		ScrollPane scrollPane = new ScrollPane(scrollTable);
+		table.add(scrollPane).expand().fill();
 	}
-
-	
-
 
 	@Override
 	public void onProfileChange(String name) {
 		table.clear();
-		StyleHelper helper = StyleHelper.getInstance();
-		ImageButton newProfile = new ImageButton(
-				helper.getImageButtonStyle("widgets/dummy-icon"));
-		newProfile.addListener(new ProfileSetNameScreenClickListener());
-		
-		for (Profile profile : profileController.getAllProfiles()) {
-			TextButton profileButton = new TextButton(profile.getName(),
-					helper.getTextButtonStyle());
-			table.add(profileButton).width(300).uniform();
-			profileButton.addListener(new ClickListener() {
-				@Override
-				public void clicked(InputEvent event, float x, float y) {
-					TextButton target = (TextButton) event.getListenerActor();
-					profileController.changeCurrentProfile(target.getText().toString());
-					game.showMainMenuScreen(SelectProfileScreen.this);
-				}
-			});
-		}
-		table.add(newProfile).uniform();
-		
+		fillTable();
 	}
-	
+
+	private class ChangeProfileClickListener extends ClickListener {
+		@Override
+		public void clicked(InputEvent event, float x, float y) {
+			TextButton target = (TextButton) event.getListenerActor();
+			profileController.changeCurrentProfile(target.getText().toString());
+			game.showMainMenuScreen(SelectProfileScreen.this);
+		}
+	}
+
 }
