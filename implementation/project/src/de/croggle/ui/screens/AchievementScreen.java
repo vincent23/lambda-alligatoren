20b7c86a0c1affbc5f1e4955276e27e959ec2d91
Tobias Hornberger <saibot1207@googlemail.com>
2014-01-29 14:36:37 +0100

    Start implementing AchievementScreen

--- implementation/project/src/de/croggle/ui/screens/AchievementScreen.java
+++ implementation/project/src/de/croggle/ui/screens/AchievementScreen.java
@@ -2,10 +2,13 @@ package de.croggle.ui.screens;
 
 import static de.croggle.data.LocalizationHelper._;
 
+import java.util.List;
+
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 import de.croggle.AlligatorApp;
+import de.croggle.game.achievement.Achievement;
 import de.croggle.game.achievement.AchievementController;
 import de.croggle.ui.StyleHelper;
 
@@ -39,6 +42,23 @@ public class AchievementScreen extends AbstractScreen {
 				helper.getImageButtonStyleRound("widgets/icon-back"));
 		Label achievements = new Label(_("screen_title_achievements"),
 				helper.getLabelStyle());
+		ImageButton next = new ImageButton(helper.getDrawable("widget/icon-next"));
+		ImageButton previous = new ImageButton(helper.getDrawable("widget/icon-back"));
+		
+		List<Achievement> availableAchievements = achievementController.getAvailableAchievements();
+		for( Achievement achievement : availableAchievements) {
+			for( int i = 0; i < achievement.getNumberOfStages(); i++) {
+				ImageButton achievementButton;
+				if (i <= achievement.getIndex()) {
+					achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathachieved(i)));
+				}
+				else {
+					achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathnotachieved(i)));
+				}
+				table.add(achievementButton);
+			}
+		}
+		
 
 		// add listeners
 		back.addListener(new LogicalPredecessorListener());
