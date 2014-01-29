59696f19e6213da8eb453eb56d53d72e442cf8e7
Tobias Hornberger <saibot1207@googlemail.com>
2014-01-29 18:10:16 +0100

    Initial Layout for AchievementScreen

--- implementation/project/src/de/croggle/ui/screens/AchievementScreen.java
+++ implementation/project/src/de/croggle/ui/screens/AchievementScreen.java
@@ -2,10 +2,16 @@ package de.croggle.ui.screens;
 
 import static de.croggle.data.LocalizationHelper._;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import android.util.Log;
 
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 
 import de.croggle.AlligatorApp;
 import de.croggle.game.achievement.Achievement;
@@ -18,6 +24,8 @@ import de.croggle.ui.StyleHelper;
  */
 public class AchievementScreen extends AbstractScreen {
 
+	private Table currentTable;
+	ArrayList<Table> block;
 	private AchievementController achievementController;
 
 	/**
@@ -32,7 +40,7 @@ public class AchievementScreen extends AbstractScreen {
 	public AchievementScreen(AlligatorApp game) {
 		super(game);
 		achievementController = game.getAchievementController();
-
+		block = new ArrayList<Table>();
 		fillTable();
 	}
 
@@ -42,29 +50,50 @@ public class AchievementScreen extends AbstractScreen {
 				helper.getImageButtonStyleRound("widgets/icon-back"));
 		Label achievements = new Label(_("screen_title_achievements"),
 				helper.getLabelStyle());
-		ImageButton next = new ImageButton(helper.getDrawable("widget/icon-next"));
-		ImageButton previous = new ImageButton(helper.getDrawable("widget/icon-back"));
+		ImageButton next = new ImageButton(helper.getDrawable("widgets/icon-next"));
+		ImageButton previous = new ImageButton(helper.getDrawable("widgets/icon-back"));
 		
 		List<Achievement> availableAchievements = achievementController.getAvailableAchievements();
+		currentTable = new Table();
 		for( Achievement achievement : availableAchievements) {
+			currentTable.row();
 			for( int i = 0; i < achievement.getNumberOfStages(); i++) {
 				ImageButton achievementButton;
-				if (i <= achievement.getIndex()) {
-					achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathachieved(i)));
+				try {
+					if (i <= achievement.getIndex()) {
+						achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathachieved(i)));
+					}
+					else {
+						achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathnotachieved(i)));
+					}
+				} catch(IllegalArgumentException e) {
+					achievementButton = new ImageButton(helper.getDrawable("widgets/dummy-icon"));
+				} catch(GdxRuntimeException e) {
+					achievementButton = new ImageButton(helper.getDrawable("widgets/dummy-icon"));
 				}
-				else {
-					achievementButton = new ImageButton(helper.getDrawable(achievement.getEmblemPathnotachieved(i)));
+				if ( i  % 7 == 6) {
+					currentTable.row();
 				}
-				table.add(achievementButton);
+				
+				currentTable.add(achievementButton);
+				
 			}
+			currentTable.row();
+			Log.d("AchievementScreen", "Umbruch wegen neuem Achievement");
 		}
 		
 
 		// add listeners
 		back.addListener(new LogicalPredecessorListener());
 
+		table.defaults().width(500).height(100).space(10);
 		table.pad(30);
-		table.add(back).size(100);
 		table.add(achievements);
+		table.row();
+		ScrollPane scrollPane = new ScrollPane(currentTable);
+		table.add(scrollPane);
+		
+		
+		
 	}
 }
